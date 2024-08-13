ca3dd04a2c993c16a5201f06a4c2f9ac44f993a5
ashique <ashique12009@gmail.com>
2024-08-13 20:38:09 +0300

    db connection is working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './dbconnection.js';
 
 const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
+
+connectDB();
 
 app.listen(8080, () => console.log(`Server listening on port ${PORT}`));
